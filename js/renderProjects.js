fef7105251e06cc41801d2e04d96d0064807e665
mojib2014 <mojib_monir@yahoo.com>
2021-01-27 21:30:35 -0800

    fix the issue with project images link

--- js/renderProjects.js
+++ js/renderProjects.js
@@ -34,49 +34,4 @@ window.onload = function () {
     else if (filterValue === "all")
       cards.forEach((card) => (card.style.display = "block"));
   }
-  // Render Projects in to the DOM
-  filtered.map((project) => {
-    // Card elements
-    const card = document.createElement("div");
-    const img = document.createElement("img");
-    const cardBody = document.createElement("div");
-    const title = document.createElement("h3");
-    const description = document.createElement("p");
-    const techStackTitle = document.createElement("h3");
-    const techStackDescription = document.createElement("p");
-    const cardLinksContainer = document.createElement("div");
-    const githubBtn = document.createElement("a");
-    const liveProjectBtn = document.createElement("a");
-    const githubIcon = document.createElement("i");
-
-    // Elements inner content
-    title.textContent = project.title;
-    description.textContent = project.description;
-    techStackTitle.textContent = project.techStackTitle;
-    techStackDescription.textContent = project.techStack;
-    liveProjectBtn.textContent = "Live Project";
-
-    // Elements attributes
-    card.className = "card";
-    cardBody.className = "card-body";
-    cardLinksContainer.className = "card-link";
-    img.alt = project.title;
-    img.src = project.projectImg;
-    githubBtn.href = project.githubUrl;
-    githubIcon.className = "fab fa-github-square";
-    liveProjectBtn.href = project.projectUrl;
-
-    // Appending all the elements
-    card.appendChild(img);
-    cardBody.appendChild(title);
-    cardBody.appendChild(description);
-    cardBody.appendChild(techStackTitle);
-    cardBody.appendChild(techStackDescription);
-    githubBtn.appendChild(githubIcon);
-    cardLinksContainer.appendChild(githubBtn);
-    cardLinksContainer.appendChild(liveProjectBtn);
-    cardBody.appendChild(cardLinksContainer);
-    card.appendChild(cardBody);
-    // portfolioCardContainer.appendChild(card);
-  });
 };
